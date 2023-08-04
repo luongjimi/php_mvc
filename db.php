d95f3034f3fc31bde5cb77f654ccaeb96ce6e673
luongjimi <a1training.edu@gmail.com>
2023-08-04 13:46:40 +0700

    first commit

--- db.php
+++ db.php
@@ -1,9 +1,6 @@
 <?php 
     $conn = mysqli_connect("localhost","root","","test");
     
-    //$conn = mysqli_connect("sql109.infinityfree.com","epiz_33885533","voxwWK3VAhb","epiz_33885533_dmnv");
-    //database in hosting
-
     mysqli_set_charset($conn, 'UTF8');
     date_default_timezone_set('Asia/Ho_Chi_Minh');
     $time = date('d-m-Y H:i:s');
